790a1b8de1b52ca39d9506fc58b447ce7fa8c75a
Kaushik Ranjan <kaushikranjanrajkumar@gmail.com>
2026-03-24 16:03:32 +0530

    version 1.0

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express =require('express')
+const multer=require('multer');
+const uploadFile = require('./services/storage.service')
+const postModel = require('./models/post.model');
+
+
+
+
+const app = express();
+app.use(express.json());
+
+const upload=multer({storage:multer.memoryStorage()})
+
+app.post('/create-post',upload.single("image"),async(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    const result = await uploadFile(req.file.buffer);
+    console.log(result);
+    const post = await postModel.create({
+        image:result.url,
+        caption:req.body.caption
+    })
+    res.status(201).json({
+        message:"Post created successfully",
+        post
+    });
+})
+
+module.exports =app;
